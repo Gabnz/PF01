d1590febd8d9ac71c1eb6ea5a680c9f80798fe60
Gabriel Nunez <gabriel.nzn@gmail.com>
2013-12-20 19:21:57 -0430

    lista vista basica.

--- header.php
+++ header.php
@@ -6,11 +6,15 @@
 	<link rel="stylesheet" href="layout.css">
 </head>
 <body>
-	<div style="position:absolute; left:0px; top:0px; width:30%; background-color:green;">
+	<div style="position:absolute; left:0px; top:0px; width:30%; background-color:#2163A6;
+	border:5px solid; border-color:#70A8E2;
+	border-radius:15px;">
 		<h3 align="center">Universidad de Carabobo</h3>
-		<div align="center"><img src="img/uc.png" alt="UC" height="100"></div>
+		<div align="center"><img src="img/uc.png" alt="UC" height="100" style="margin-bottom:15px;"></div>
 	</div>
-	<div style="position:absolute; right:0px; top:0px; width:30%; background-color:blue;">
+	<div style="position:absolute; right:0px; top:0px; width:30%; background-color:#5C9970;
+	border:5px solid; border-color:#9FC1AA;
+	border-radius:15px;">
 		<h3 align="center">Integrantes</h3>
 		<ul>
 			<li>Ada Parra</li>
@@ -21,6 +25,8 @@
 		</ul>
 	</div>
 	<div>
-		<h1 align="center" style="margin-top:20%;">Metodo Simplex revisado</h1>
+		<div align="center" style="margin-top:5%;">
+	<img src="img/facyt.png" alt="UC" height="100"></div>
+		<h1 align="center" style="margin-top:5%;">Metodo Simplex revisado</h1>
 	</div>
 
--- index2.php
+++ index2.php
@@ -1,7 +1,7 @@
 <?php include 'header.php' ?>
 
 <div align="center">
-	<h1 align="center">Testeando</h1>
+	
 </div>
 
 <?php include 'footer.php' ?>
