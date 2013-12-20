03cbee8c2d2d908571bdfc92c648fc3a485635c2
Gabriel Nunez <gabriel.nzn@gmail.com>
2013-12-20 17:02:22 -0430

    agregado logo facyt.

--- footer.php
+++ footer.php
@@ -2,7 +2,10 @@
 		<div style="position:absolute; left:0px; right:0px; bottom:0px; width:100%;
 		height:15%; background-color:#545454;">
 			<div style="position:absolute; left:0px; width:25%; background-color:green;">
-			<h3 align="center">Identificacion FACyT</h3></div>
+				<h3 align="center">Identificacion FACyT</h3>
+				<div align="center"><img src="img/facyt.png" alt="UC" height="100"></div>
+			</div>
+			
 			<div style="position:absolute; right:0px; width:25%; background-color:blue;">
 				<h3 align="center">Otra cosa</h3>
 			</div>
